442b006a80f5b9c62b4bba6ecbaa574b1a1a4531
Michal Kloczko <michal.kloczko@michsoft.pl>
2014-02-10 23:23:40 +0100

    error parser recognizing error messages

---
+++ PydioSDK/PydioSDK/Classes/ResponseParsers/ErrorResponseParserDelegate.m
@@ -0,0 +1,66 @@
+//
+//  ErrorResponseParserDelegate.m
+//  PydioSDK
+//
+//  Created by ME on 10/02/14.
+//  Copyright (c) 2014 MINI. All rights reserved.
+//
+
+#import "ErrorResponseParserDelegate.h"
+
+
+static NSString * const TREE_NODE = @"tree";
+static NSString * const MESSAGE_NODE = @"message";
+static NSString * const TYPE_ATTRIBUTE = @"type";
+
+typedef NS_ENUM(NSUInteger,State) {
+    InitialState,
+    ExpectErrorMessageStart,
+    ExpectErrorMessageEnd,
+    IgnoreState
+};
+
+@interface ErrorResponseParserDelegate ()
+@property (readwrite,nonatomic,strong) NSString* errorMessage;
+
+@property (nonatomic,assign) State state;
+@property (nonatomic,strong) NSString *buffer;
+@end
+
+
+@implementation ErrorResponseParserDelegate
+
+-(instancetype)init {
+    self = [super init];
+    if (self) {
+        self.state = InitialState;
+    }
+    return self;
+}
+
+#pragma mark - NSXMLParserDelegate
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict {
+    if (self.state == InitialState && [elementName isEqualToString:TREE_NODE]) {
+        self.state = ExpectErrorMessageStart;
+    } else if (self.state == ExpectErrorMessageStart && [elementName isEqualToString:MESSAGE_NODE] && [attributeDict valueForKey:TYPE_ATTRIBUTE]) {
+        self.state = ExpectErrorMessageEnd;
+    } else if (self.state != IgnoreState) {
+        [parser abortParsing];
+    }
+
+    self.buffer = @"";
+}
+
+-(void)parser:(NSXMLParser*)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName {
+    if (self.state == ExpectErrorMessageEnd) {
+        self.errorMessage = [NSString stringWithString:self.buffer];
+        self.state = IgnoreState;
+    }
+}
+
+-(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
+    self.buffer = [self.buffer stringByAppendingString:string];
+}
+
+@end
---
+++ PydioSDK/PydioSDKTests/ResponseParsers/ErrorResponseParserDelegateTests.m
@@ -0,0 +1,67 @@
+//
+//  ErrorResponseParserDelegateTests.m
+//  PydioSDK
+//
+//  Created by ME on 10/02/14.
+//  Copyright (c) 2014 MINI. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#define HC_SHORTHAND
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
+#define MOCKITO_SHORTHAND
+#import <OCMockitoIOS/OCMockitoIOS.h>
+
+#import "ErrorResponseParserDelegate.h"
+#import "XCTestCase+XMLFixture.h"
+
+
+@interface ErrorResponseParserDelegateTests : XCTestCase
+@property (nonatomic,strong) ErrorResponseParserDelegate* parserDelegate;
+@end
+
+@implementation ErrorResponseParserDelegateTests
+
+- (void)setUp
+{
+    [super setUp];
+    self.parserDelegate = [[ErrorResponseParserDelegate alloc] init];
+}
+
+- (void)tearDown
+{
+    self.parserDelegate = nil;
+    [super tearDown];
+}
+
+- (void)testShouldParseErrorMessage
+{
+    NSXMLParser *parser = [self parserWithFixture:@"error_response.xml" delegate:self.parserDelegate];
+    
+    BOOL result = [parser parse];
+    
+    assertThatBool(result,equalToBool(YES));
+    assertThat(self.parserDelegate.errorMessage,equalTo(@"Example error message!"));
+}
+
+-(void)testShouldNotRecognizeOtherResponsesAsErrorResponse
+{
+    NSArray *responses = @[
+                           @"login_response.xml",
+                           @"get_registers_response.xml",
+                           @"ls_response2.xml"
+                           ];
+    
+    for (NSString *file in responses) {
+        self.parserDelegate = [[ErrorResponseParserDelegate alloc] init];
+        NSXMLParser *parser = [self parserWithFixture:file delegate:self.parserDelegate];
+        
+        BOOL result = [parser parse];
+        
+        assertThatBool(result,equalToBool(NO));
+        XCTAssertNil(self.parserDelegate.errorMessage, @"File: %@ should not be recognized as error response",file);
+        self.parserDelegate = nil;
+    }
+}
+
+@end
