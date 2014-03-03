b5c49cc3943d42ce1b2339dc74e4a1f3c4674376
Michal Kloczko <michal.kloczko@michsoft.pl>
2014-03-03 21:38:57 +0100

    Added example application as separate project

---
+++ ExampleApplication/PydioClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PydioClient
+//
+//  Created by Michal Kloczko on 02/03/14.
+//  Copyright (c) 2014 Pydio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
